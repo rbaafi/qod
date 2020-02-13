4a5403b1bd9c9f4e2b7707598a67d521cd82d735
Richmond <rbaafi@cnm.edu>
2020-02-13 15:12:02 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/qod/model/entity/Quote.java
@@ -0,0 +1,101 @@
+package edu.cnm.deepdive.qod.model.entity;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(
+    indexes = {
+        @Index(columnList = "created")
+    }
+)
+public class Quote {
+
+  @NonNull
+  @Id@GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(name = "quote_id", columnDefinition = "CHAR(16) FOR BIT DATA",
+      nullable = false, updatable = false)
+  private UUID id;
+
+  @NonNull
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false, updatable = false)
+  private Date created;
+
+  @NonNull
+  @UpdateTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  private Date updated;
+
+  @NonNull
+  @Column(length = 4096, nullable = false, unique = true)
+  private String text;
+
+  @NonNull
+  @ManyToMany(
+      fetch = FetchType.LAZY,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
+  )
+  @JoinTable(
+      joinColumns = {
+          @JoinColumn(name = "quote_id")
+      },
+      inverseJoinColumns = {
+          @JoinColumn(name = "source_id")
+      }
+  )
+  @OrderBy("name ASC")
+  private List<Source> sources = new LinkedList<>();
+
+  @NonNull
+  public UUID getId() {
+    return id;
+  }
+
+  @NonNull
+  public Date getCreated() {
+    return created;
+  }
+
+  @NonNull
+  public Date getUpdated() {
+    return updated;
+  }
+
+  @NonNull
+  public String getText() {
+    return text;
+  }
+
+  public void setText(@NonNull String text) {
+    this.text = text;
+  }
+
+  @NonNull
+  public List<Source> getSources() {
+    return sources;
+  }
+}
